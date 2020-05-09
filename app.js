e359babb8dfc2667d22300060f9dbb905f7ae7dc
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-05-09 02:43:25 +0100

    chore : add initial commit for backend

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+require('dotenv').config()
+const mongoose = require("mongoose");
+
+const userRoutes = require('./routes/user');
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.use('/api/v1/user', userRoutes);
+
+
+app.use((req, res) => {
+    res.send("<h1>Welcome to Alert API page</h1>");
+});
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose
+    .connect(
+        "mongodb+srv://josh:jesus000@cluster0-hziu4.mongodb.net/alert?retryWrites=true&w=majority", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    )
+    .then(() => {
+        console.log("Database connected");
+        app.listen(port);
+    })
+    .catch((err) => console.log(err));
+
+module.exports = app
