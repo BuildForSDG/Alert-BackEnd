406f95f554abd0ae204510db3188db340baca5f0
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-05-11 23:45:11 +0100

    feature: add login endpoint for regular users

--- routes/regularUser.js
+++ routes/regularUser.js
@@ -7,6 +7,7 @@ const userController = require('../controllers/regularUser');
 
 
 router.post('/signup', validation.signUpValidationRules(), validation.validate, userController.signUp);
+router.post('/login', validation.loginValidationRules(), validation.validate, userController.login);
 
 
 module.exports = router;
