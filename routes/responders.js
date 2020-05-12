7ac80fd20c51ad425bc52d11424cc5b64e70ff5c
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-05-12 01:35:05 +0100

    feature: add get responders details endpoint

--- routes/responders.js
+++ routes/responders.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
+const auth = require('../middleware/auth');
 const validation = require('../middleware/validate');
 
 const userController = require('../controllers/responders');
 
-
+router.get('/:id', auth.authentication, userController.getDetails);
 router.post('/signup', validation.responderssignUpValidationRules(), validation.validate, userController.signUp);
 router.post('/login', validation.loginValidationRules(), validation.validate, userController.login);
 
